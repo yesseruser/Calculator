524bd7827e211eb41f654d1088f853a3c368ff34
yesseruser <matej2.petricek@outlook.cz>
2021-08-23 19:24:05 +0200

    Přidejte soubory projektu.

---
+++ Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static int firstNumber;
+        static int Operator;
+        static int secondNumber;
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            firstNumber = Convert.ToInt32(Console.ReadLine());
+            con
+            Console.ReadKey();
+        }
+    }
+}
